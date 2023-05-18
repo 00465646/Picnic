33ded28fa9454c5aac922f8ecce42ce51c9f4510
Tibedox <tibedox@mail.ru>
2023-05-18 20:37:10 +0600

    komment

--- core/src/com/mygdx/game/Picnic.java
+++ core/src/com/mygdx/game/Picnic.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Picnic extends ApplicationAdapter {
 	SpriteBatch batch;
-	//Texture img;
+	Texture img; // коммент
 	
 	@Override
 	public void create () {
